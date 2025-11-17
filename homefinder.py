2f07faffa6371c3c7cbe5c4f3f9517146a3f564a
Saishravan Muthukrishnan <saishravanm@gmail.com>
2025-11-16 19:28:50 -0600

    Changed Chainlit UI and added logo and favicom

--- homefinder.py
+++ homefinder.py
@@ -54,7 +54,6 @@ def remove_PII(text):
     response = comprehend_client.detect_pii_entities(Text=text,LanguageCode=language_code)
     redacted_text = list(text)
     for entity in response['Entities']:
-        if entity['Type'] != 'ADDRESS':
             for i in range(entity['BeginOffset'],entity['EndOffset']):
                 redacted_text[i] = '*'
     redacted_text = "".join(redacted_text)
@@ -63,7 +62,6 @@ def remove_PII(text):
 # SerpAPI function removed - now using TavilySearch
 
 @cl.on_chat_start
-def init():
     #generate user session id for the session
     session_id = uuid.uuid4()
     cl.user_session.set("id",session_id)
@@ -136,7 +134,6 @@ def init():
 #define agentic prompt - simplified and more conversational
     agent_prompt = ChatPromptTemplate.from_messages(
             [
-                ("system", f"You are HomeFinder, an empathetic AI assistant that helps people in the DFW area experiencing homelessness find resources. Recognize the user's language that they're speaking in (if it's hard to tell what language they're speaking in, the default language is English), and translate all your responses (and responses coming from any tools) during the conversation in the user speaking language. Before searching for resources, make sure you speak to the user empathetically about their situation, and when it seems clear that they just want the resources and not a detailed conversation about their needs, search for resources with the information you have. When searching for resources, send the search request in the language that the user is speaking in, use the knowledge base FIRST (ensure that the user provided parameters such as location adequately MATCH the resources in the knowledge base, don't just blatantly copy info from it), and if there's still any information still missing you can use the internet for current information. The resources returned must be as close as possible in either proximity and/or need to the user provided location/scenario. Be warm, understanding, and helpful. Ask follow-up questions to further refine your searches before using the internet or knowledge base (ie: location, more info about situation etc) to make it more of a personal experience. Focus on practical help like shelters, food, healthcare, and other essential services. Do a sentiment analysis on each user response and base your responses/resources on how the user seems to be feeling. Don't sound robotic, sound conversational. YOU MUST USE {resource_format} as your format for searching and showing the user the information you found.If the user has seemed to provide any personal identifiable information, kindly request them to not include anything as such (pii information you recieve should be donated by multiple *'s)"),                MessagesPlaceholder(variable_name="history"),
                 ("human", "{question}"),
                 ("system","{agent_scratchpad}")
             ]
@@ -197,6 +194,7 @@ async def main(message: cl.Message):
     try:
         #get agent response with debugging
         print(f"DEBUG: User message: {message.content}")
+<<<<<<< Updated upstream
         
         #get the PII removed text
         pii_removed_message = remove_PII(message.content)
